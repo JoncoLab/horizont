b9e92c627c60d0339da4e3717612838985673761
Alex <ohanchak@gmail.com>
2019-03-13 14:24:46 +0200

    user-table

--- src/components/users-table/users-table.js
+++ src/components/users-table/users-table.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import './users-table.css';
 
-const UsersTable = () => <form><caption>UploadFiles</caption></form>;
+const UsersTable = () => <form>
+    <caption>UploadFiles</caption>
+</form>;
+
+//    need DB connection!!!
+
+const UserList = ({name, profile}) => {
+    return <span>{name},{profile}</span>
+};
+
+const AllUsers = ({allUsers}) => {
+    const users = allUsers.map((user) => {
+        const {id, ...userProps} = user;
+        return (
+            <li key={id}>
+                <UserList {...userProps}/>
+            </li>
+        )
+    });
+    return (
+        <ul>
+            {users}
+        </ul>
+    );
+};
 
 export default UsersTable;
